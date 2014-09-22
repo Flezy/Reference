db4572104dc28ee6c94d6039789958009e1c5840
Farkas Levente <levnzz@gmail.com>
2014-09-22 23:58:47 +0200

    hello

--- first_django/polls/admin.py
+++ first_django/polls/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
+
+class ChoiceinLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+        
+    
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                  {'fields':  ['question_text']}),
-        ('Date information',    {'fields':  ['pubdate'],            'classes': ['collapse']})
-      ]
+        ('Date information',    {'fields':  ['pubdate']})
+    ]
+    inlines = [ChoiceinLine]
+    list_display = ('question_text', 'pubdate', 'was_published_recently')
 admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
